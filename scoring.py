2af467a894832c7e2ffc62529604447e4268f099
Gasquez Florian <m@fy.to>
2018-04-02 19:22:45 +0200

    fix scoring

--- scoring.py
+++ scoring.py
@@ -12,18 +12,19 @@ import regex as re
 from misc_utils import safe_exp, format_spm_text
 
 class Scoring(object):
-	_poncts = re.compile(r'[\.!\?;]$')
-	_emotes_utf8 = re.compile(r'[\u263a-\U0001f645]')
-	_emotes = [':)', ':(', ':D', ':P', ':p', '^^', '^_^', 'o_O', 'O_o', ':o', ':/', '/o/']
+	_poncts = re.compile(r'[\.!\?;]$') # for end lines
+	_emotes_utf8 = re.compile(r'[\u263a-\U0001f645]') # smileyyyy!
+	_emotes = [':)', ':(', ':D', ':P', ':p', '^^', '^_^', 'o_O', 'O_o', ':o', ':/', '/o/'] # oO
 
 	_subsentences = [
+		# (score, question, answer)
 		(-2, None, re.compile('(?i)I don\'t know')),
 		(-2, None, re.compile('(?i)I don\'t think')),
 		(-10, None, re.compile('(?i)no, no, no, no')),
 		(-10, None, re.compile('(?i)yes, yes, yes, yes')),
 		(3, re.compile('(?i)you( are|\'re)|are you'), re.compile('(?i) I\'m')),
 		(3, re.compile('(?i)how old'), re.compile('(?i)years old|CALL_NUM_')),
-		(3, re.compile('(?i)what colo'), re.compile('(?i)blue|yellow|red|orange|grey|green|pink|black|white|turquoise')), # color list plz
+		(3, re.compile('(?i)what color'), re.compile('(?i)blue|yellow|red|orange|grey|green|pink|black|white|turquoise')), # color list plz
 		(3, re.compile('(?i)how many'), re.compile('CALL_NUM_'))
 	]
 
@@ -58,16 +59,18 @@ class Scoring(object):
 
 	def check_score(self, awnser, index):
 		score = 15
-		if index in [0,1]:
+
+		# use default score
+		if index in [0,1]: 
 			score += 2
 		if index in [2,3]:
 			score += 1
 		if index in [4,5]:
 			score += .5
 
-		score += self.check_ending(awnser[-1:len(awnser)])
-		score += self.check_subsentence(awnser)
-		score += self.check_smilarity(awnser)
+		score += self.check_ending(awnser[-1:len(awnser)]) # last char checkings
+		score += self.check_subsentence(awnser) # check _subsentences
+		score += self.check_smilarity(awnser) # check similarity
 		return score
 
 	def check_subsentence(self, awnser):
@@ -92,15 +95,3 @@ class Scoring(object):
 			return 3
 		else:
 			return -2
-
-if __name__ == '__main__':
-	_ending = re.compile(r'[\.!\?;]$')
-	_emotes_utf8 = re.compile(r'[\u263a-\U0001f645]')
-	s1 = "blah 4 xzuyguhbc 😺."
-	s2 = " reoroeor 😺 zrzer zrez"
-
-	s1_end = s1[-1:len(s1)]
-	s2_end = s2[-1:len(s2)]
-
-	print (s1_end, s2_end)
-	print(re.search(_ending, s1_end), re.search(_ending, s2_end))
